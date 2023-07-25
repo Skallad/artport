bcdac78335a8a6418a0f1778fb470562569e2cc4
Skallad <gramatikofski.elena@gmail.com>
2023-07-25 20:14:03 +0000

    Alter navbar, remove dropdown, put in container

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,9 @@ import {Navbar, Container, Nav} from 'react-bootstrap'
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="md" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Xrt-Port</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
